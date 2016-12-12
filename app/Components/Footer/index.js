9f3d3ad5a433e8a27f123512348388c681f3edcb
catcxj <catcxj@163.com>
2016-12-12 11:49:59 +0800

    去掉footer错误协议。

--- app/Components/Footer/index.js
+++ app/Components/Footer/index.js
@@ -7,9 +7,6 @@ function Footer() {
 	return (
 		<footer className={styles.footer}>
 			<section>
-				<p>这个项目基于 MIT 开源协议.</p>
-			</section>
-			<section>
 				<p>制作： 浙江华东工程数字技术有限公司.</p>
 			</section>
 		</footer>
