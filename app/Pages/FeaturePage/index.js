accdb702504d6f55216d6e825af21fe6b8d5612c
xueying <catcxj@163.com>
2016-12-19 20:16:48 +0800

    添加 LICENSE 开源协议注释

--- app/Pages/FeaturePage/index.js
+++ app/Pages/FeaturePage/index.js
@@ -1,8 +1,11 @@
-/*
+/**
+ * Copyright (c) 2016-present, ecidi.
+ * All rights reserved.
+ * 
+ * This source code is licensed under the GPL-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
  * FeaturePage
- *
- * at the '/features' route
- *
  */
 
 import React from 'react';
--- app/Pages/HomePage/fetch.js
+++ app/Pages/HomePage/fetch.js
@@ -1,11 +1,15 @@
 /**
+ * Copyright (c) 2016-present, ecidi.
+ * All rights reserved.
+ * 
+ * This source code is licensed under the GPL-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
  * 从Github获取user repositories
  */
 import request from 'Utils/request';
 
-/**
- * Github 响应 request/response 处理
- */
+// Github 响应 request/response 处理
 export function githubData(username, success, err) {
     const requestURL = `https://api.github.com/users/${username}/repos?type=all&sort=updated`;
 
